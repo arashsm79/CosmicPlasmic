17f818615635207a026a43860719b5936b2486d6
Arash Sal Moslehian <57039957+arashsm79@users.noreply.github.com>
2024-06-11 10:14:20 +0200

    Update build.sh

--- config/scripts/build.sh
+++ config/scripts/build.sh
@@ -5,11 +5,3 @@
 # builds actually ran successfully without any errors!
 set -oue pipefail
 
-# Set up display manager
-rm -f /etc/systemd/system/display-manager.service
-ln -s /usr/lib/systemd/system/cosmic-greeter.service /etc/systemd/system/display-manager.service
-
-# System services
-systemctl enable podman.socket
-systemctl enable docker.socket
-systemctl enable cosmic-greeter.service
